8621ddbc06e989d276d2e4f187df3709eda49107
Habiba618 <82892084+Habiba618@users.noreply.github.com>
2023-12-13 10:53:00 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double a,b;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("eded 1: ");
+        a = sc.nextDouble();
+        System.out.print("eded 2: ");
+        b = sc.nextDouble();
+        System.out.println("1 -> +");
+        System.out.println("2 -> -");
+        System.out.println("3 -> *");
+        System.out.println("4 -> /");
+        int value = sc.nextInt();
+        switch (value) {
+            case 1:
+                System.out.println(a+"+"+b +"="+ (a+b));
+                break;
+            case 2:
+                System.out.println(a+"-"+b +"="+ (a-b));
+                break;
+            case 3:
+                System.out.println(a+"/"+b +"="+ (a/b));
+                break;
+          case 4:
+               if(b!=0) {
+                   System.out.println(a + "*" + b + "=" + (a * b));
+               }else{
+                   System.out.println("0-a bolme yoxdur");
+               }
+              break;
+            default:
+                System.out.println("Secim dogru deyil");
+
     }
+}
 }
